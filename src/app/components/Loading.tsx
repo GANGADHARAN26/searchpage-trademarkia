05333aa5a53e98905dd2eb8fa41922d9ed77ecfc
GANGADHARAN-ls <gangadharan.a@lunarasolutions.in>
2025-04-10 11:53:53 +0530

    added loadings

--- src/app/components/Loading.tsx
+++ src/app/components/Loading.tsx
@@ -1,5 +1,11 @@
+import Modal from "./Model";
+import {OrbitProgress} from "react-loading-indicators"
 // app/dashboard/loading.tsx
 export default function Loading() {
-    return <p className="text-center text-gray-500">Loading...</p>;
+    return (<Modal isOpen={true} handleClose={() => {}}>
+      <p className="flex justify-center items-center text-center text-gray-500 w-full h-full p-20">
+      <OrbitProgress dense color="#E6670D" size="medium" text="" textColor="" />
+      </p>
+    </Modal>);
   }
   
